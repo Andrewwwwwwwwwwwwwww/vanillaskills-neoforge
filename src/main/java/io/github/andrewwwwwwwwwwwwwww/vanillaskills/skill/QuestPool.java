c92bd1e8b6bdc13cb70548c9292938b0966ba89f
Andrewwwwwwwwwwwwwww <andrewjsikula@gmail.com>
2026-07-04 17:07:40 -0700

    1.2.1: starter reward -1 (parity)

--- src/main/java/io/github/andrewwwwwwwwwwwwwww/vanillaskills/skill/QuestPool.java
+++ src/main/java/io/github/andrewwwwwwwwwwwwwww/vanillaskills/skill/QuestPool.java
@@ -17,21 +17,21 @@ public final class QuestPool {
      * unrenderable and would make graduation impossible. To change a quest, replace its entry.
      */
     public static final List<Quest> STARTER = List.of(
-            new Quest(Quest.Type.GATHER, "minecraft:stick", 32, 2, "Gather 32 Sticks"),
-            new Quest(Quest.Type.GATHER, "minecraft:cobblestone", 64, 2, "Gather 64 Cobblestone"),
-            new Quest(Quest.Type.GATHER, "minecraft:coal", 16, 3, "Gather 16 Coal"),
-            new Quest(Quest.Type.GATHER, "minecraft:bread", 16, 3, "Bake 16 Bread"),
-            new Quest(Quest.Type.GATHER, "minecraft:leather", 16, 5, "Gather 16 Leather"),
-            new Quest(Quest.Type.GATHER, "minecraft:copper_ingot", 32, 4, "Smelt 32 Copper Ingots"),
-            new Quest(Quest.Type.GATHER, "minecraft:iron_ingot", 16, 5, "Smelt 16 Iron Ingots"),
-            new Quest(Quest.Type.GATHER, "minecraft:gold_ingot", 8, 5, "Smelt 8 Gold Ingots"),
-            new Quest(Quest.Type.GATHER, "minecraft:diamond", 4, 6, "Gather 4 Diamonds"),
-            new Quest(Quest.Type.KILL, "minecraft:zombie", 10, 4, "Slay 10 Zombies"),
-            new Quest(Quest.Type.KILL, "minecraft:skeleton", 10, 4, "Slay 10 Skeletons"),
-            new Quest(Quest.Type.KILL, "minecraft:creeper", 5, 5, "Slay 5 Creepers"),
-            new Quest(Quest.Type.GATHER, "minecraft:bone", 16, 4, "Gather 16 Bones"),
-            new Quest(Quest.Type.GATHER, "minecraft:string", 8, 4, "Gather 8 String"),
-            new Quest(Quest.Type.SKILL, "", 10, 6, "Unlock 10 Skills")
+            new Quest(Quest.Type.GATHER, "minecraft:stick", 32, 1, "Gather 32 Sticks"),
+            new Quest(Quest.Type.GATHER, "minecraft:cobblestone", 64, 1, "Gather 64 Cobblestone"),
+            new Quest(Quest.Type.GATHER, "minecraft:coal", 16, 2, "Gather 16 Coal"),
+            new Quest(Quest.Type.GATHER, "minecraft:bread", 16, 2, "Bake 16 Bread"),
+            new Quest(Quest.Type.GATHER, "minecraft:leather", 16, 4, "Gather 16 Leather"),
+            new Quest(Quest.Type.GATHER, "minecraft:copper_ingot", 32, 3, "Smelt 32 Copper Ingots"),
+            new Quest(Quest.Type.GATHER, "minecraft:iron_ingot", 16, 4, "Smelt 16 Iron Ingots"),
+            new Quest(Quest.Type.GATHER, "minecraft:gold_ingot", 8, 4, "Smelt 8 Gold Ingots"),
+            new Quest(Quest.Type.GATHER, "minecraft:diamond", 4, 5, "Gather 4 Diamonds"),
+            new Quest(Quest.Type.KILL, "minecraft:zombie", 10, 3, "Slay 10 Zombies"),
+            new Quest(Quest.Type.KILL, "minecraft:skeleton", 10, 3, "Slay 10 Skeletons"),
+            new Quest(Quest.Type.KILL, "minecraft:creeper", 5, 4, "Slay 5 Creepers"),
+            new Quest(Quest.Type.GATHER, "minecraft:bone", 16, 3, "Gather 16 Bones"),
+            new Quest(Quest.Type.GATHER, "minecraft:string", 8, 3, "Gather 8 String"),
+            new Quest(Quest.Type.SKILL, "", 10, 5, "Unlock 10 Skills")
     );
 
     // Rewards are in Quest Shards, tiered by difficulty (easy 4-5, medium 6-7, hard 8-9) so a full
